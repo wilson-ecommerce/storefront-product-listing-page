d67d97ea5bb61b7e3fb44e7f73b0197d732d5645
Tiffany Pecaud <pecaud.tiffany.external@amersports.com>
2025-03-27 14:37:33 +0100

    WAF-1125 - Plp images lazy load

--- src/components/ImageHover/ImageHover.tsx
+++ src/components/ImageHover/ImageHover.tsx
@@ -10,6 +10,8 @@ it.
 import { FunctionComponent } from 'preact';
 
 import './ImageHover.css';
+import { useRef, useEffect } from 'preact/compat';
+import { useIntersectionObserver } from '../../utils/useIntersectionObserver';
 
 export interface ImageCarouselProps {
   images: string[] | { src: string; srcset: any }[];
@@ -21,19 +23,36 @@ export const ImageHover: FunctionComponent<ImageCarouselProps> = ({
     const backImage = images.length > 1 
     ? (typeof images[1] === 'object' ? images[1].src : images[1])
     : '';
+    const imageRef = useRef<HTMLDivElement>(null);
+    const entry = useIntersectionObserver(imageRef, { rootMargin: '200px' });
+
+    useEffect(() => {
+      if (!entry) return;
+
+      if (entry?.isIntersecting) {
+        var image = entry.target;
+        image.classList.remove("lazy");
+
+        if (backImage) {
+          const preloadLink = document.createElement('link');
+          preloadLink.rel = 'preload';
+          preloadLink.as = 'image';
+          preloadLink.href = backImage;
+          document.head.appendChild(preloadLink);
+        }
+      }
+    }, [entry]);
 
     return (
       <>
-        {backImage && (
-          <link rel="preload" as="image" href={backImage} />
-        )}
-          <meta itemProp="image" content={typeof images[0] === 'object' ? images[0].src : images[0]} />
+        <meta itemProp="image" content={typeof images[0] === 'object' ? images[0].src : images[0]} />
         <div class="relative w-full pb-[122.22%]">
-          <div class={`ds-sdk-product-image ${backImage ? 'hover-enabled' : ''} absolute h-full w-full m-auto bg-cover bg-no-repeat bg-position-center`}
+          <div class={`ds-sdk-product-image ${backImage ? 'hover-enabled' : ''} absolute h-full w-full m-auto bg-cover bg-no-repeat bg-position-center lazy`}
               style={{
                 '--image-url': `url('${typeof images[0] === 'object' ? images[0].src : images[0]}')`,
                 '--hover-url': `url('${backImage}')`,
               }}
+              ref={imageRef}
             />
         </div>
       </>
